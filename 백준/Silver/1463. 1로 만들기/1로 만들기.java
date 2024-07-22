d5e97ae03000cbf5603dfdf18f7be30249b67de5
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-07-22 19:53:03 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -5,46 +5,16 @@ import java.util.*;
 
 
 class Main {
-    public static class pair{
-        int num;
-        int cnt;
-
-        pair(int num, int cnt){
-            this.num = num;
-            this.cnt = cnt;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-
-        int x = Integer.parseInt(br.readLine());
-        Queue<pair> q = new LinkedList<>(); // 1로 만들어야함.
-        q.add(new pair(x, 0));
-
-        while(!q.isEmpty()){
-            int num = q.peek().num;
-            int cnt = q.poll().cnt;
-            if(num == 1) {
-                System.out.println(cnt);
-                return;
-            }
-            int tmpnum;
-            if(num % 3 == 0){
-                tmpnum = num;
-                tmpnum /= 3;
-                q.add(new pair(tmpnum, cnt+1));
-            }
-            if(num % 2 == 0){
-                tmpnum = num;
-                tmpnum /= 2;
-                q.add(new pair(tmpnum, cnt+1));
-            }
-            tmpnum = num;
-            tmpnum-=1;
-            q.add(new pair(tmpnum, cnt+1));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        arr[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            if (i % 3 == 0) arr[i] = Math.min(arr[i], arr[i / 3] + 1);
         }
+        System.out.println(arr[n]);
     }
 }
