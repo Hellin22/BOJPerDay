3c39d00c1e6fad78d8561122251e44a5c50f3d41
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-08-01 12:33:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int answer = 0;
-        int fiveMogs = n/5;
-        for(int i = 0; i < 3; i++) {
-            // 5의 개수를 하나씩 빼면서 확인.
-            if((n - fiveMogs*5)% 3 == 0){
-                answer = fiveMogs + (n - fiveMogs*5)/3;
-                break;
-            }
-            fiveMogs-=1;
-            if(fiveMogs== -1) break;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        if(N >= 3) dp[3] = 1;
+        if(N >= 5) dp[5] = 1;
+        for (int i = 6; i <= N; i++) {
+            if(dp[i-3] == 0 || dp[i-5] == 0) dp[i] = Math.max(dp[i-3], dp[i-5]) + 1;
+            else dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+            if(dp[i] == 1) dp[i] = 0;
         }
-        if(answer == 0) System.out.println(-1);
-        else System.out.println(answer);
+        if(dp[N] == 0) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(dp[N]);
     }
 }
