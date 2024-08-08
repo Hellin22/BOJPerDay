b5c285c77298fa77e18fbb00bfb149e62919e025
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-08-08 19:44:50 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 608 ms, Memory: 63560 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] ss = br.readLine().split(" ");
+        int n = Integer.parseInt(ss[0]);
+        int m = Integer.parseInt(ss[1]);
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, String> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        for(int i = 0; i < m; i++) sb.append(map.get(br.readLine()) + "\n");
+
+        System.out.println(sb);
+    }
+}
