5bddcd635a2e8b2c2e1c49bd42be60ea7b72d166
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-08-11 23:08:45 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,21 +1,16 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.io.IOException;
-class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
-            arr.add(s.charAt(i)-'0');
-        }
+public class  Main{ 
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] n = br.readLine().split("");
+		String[] arr = new String[n.length];
+		Arrays.sort(n);
+		for(int i=0;i<n.length;i++) {
+			arr[i] = n[n.length-i-1];
+		}
+		for(String item : arr)
+			System.out.print(item);
+	}     
+} 
 
-        Collections.sort(arr, Collections.reverseOrder());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < arr.size(); i++){
-            sb.append(arr.get(i));
-        }
-        System.out.println(sb);
-    }
-}
