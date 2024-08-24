71adb5f9312b955cc179d30b04b250f0695f93e5
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-08-24 21:34:22 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
