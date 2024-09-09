4ce5f7e97dec2312fe05d1bacbcabb055707db29
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-09-09 14:24:17 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,65 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-
-class Main {
-
-    public static class pair{
-        int x;
-        int y;
-        int cnt;
-        pair(int x, int y, int cnt){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
-    }
-
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-
-
+public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        String[] strs = br.readLine().split(" ");
+        int n = Integer.parseInt(strs[0]);
+        int m = Integer.parseInt(strs[1]);
 
-        int[][] arr = new int[n + 1][m + 1]; // 1,1부터 시작
-        int[][] isvisited = new int [n+1][m+1];
-        for (int i = 1; i <= n; i++) {
+        int[][] arr = new int[n+1][m+1];
+        int[][] visited = new int[n+1][m+1];
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        for(int i = 1; i <= n; i++){
             String str = br.readLine();
             for(int j = 0; j < str.length(); j++){
                 arr[i][j+1] = str.charAt(j) - '0';
             }
         }
 
-        Queue<pair> q = new LinkedList<>();
-        q.add(new pair(1, 1, 1));
-        isvisited[1][1] = 1;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{1, 1, 1});
+        visited[1][1] = 1;
 
         while(!q.isEmpty()){
-            int x = q.peek().x;
-            int y = q.peek().y;
-            int cnt = q.poll().cnt;
-            
-            for(int i = 0; i < 4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            int[] cur = q.poll();
 
-                if(nx <= 0 || ny <= 0 || nx >= n+1 || ny >= m+1
-                        || isvisited[nx][ny] == 1 || arr[nx][ny] == 0) continue;
-                
+            for(int i = 0; i < 4; i++){
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if(nx <= 0 || ny <= 0 || nx > n || ny > m
+                || visited[nx][ny] == 1 || arr[nx][ny] == 0) continue;
+
                 if(nx == n && ny == m){
-                    System.out.println(cnt + 1);
+                    System.out.println(cur[2]+1);
                     return;
                 }
-
-                q.add(new pair(nx, ny, cnt+1));
-                isvisited[nx][ny] = 1;
+                q.add(new int[]{nx, ny, cur[2] + 1});
+                visited[nx][ny] = 1;
             }
         }
     }
