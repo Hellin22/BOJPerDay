9bc73ee368de19fa28b3466657d3a039a0d29b91
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-10-24 12:50:16 +0900

    [Gold V] Title: 내려가기, Time: 380 ms, Memory: 52880 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,39 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr = new int[100001][3];
-    static int[][] max_min = new int[100001][3];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+         int[][] arr = new int[n+1][3];
+         int[][] maxx = new int[n+1][3];
+         int[][] minn = new int[n+1][3];
         int max = -1; int min = 10;
         for (int i = 1; i <= n; i++) {
-            String[] strs = br.readLine().split(" ");
-            arr[i][0] = Integer.parseInt(strs[0]);
-            arr[i][1] = Integer.parseInt(strs[1]);
-            arr[i][2] = Integer.parseInt(strs[2]);
-        }
-
-        for(int i = 1; i <= n; i++){
-            max_min[i][0] = Math.max(max_min[i-1][0], max_min[i-1][1]) + arr[i][0];
-            max_min[i][1] = Math.max(max_min[i-1][0], Math.max(max_min[i-1][1], max_min[i-1][2])) + arr[i][1];
-            max_min[i][2] = Math.max(max_min[i-1][1], max_min[i-1][2]) + arr[i][2];
-        }
-        max = Math.max(max_min[n][0], Math.max(max_min[n][1], max_min[n][2]));
-        for(int i = 1; i <= n; i++){
-            max_min[i][0] = 0;
-            max_min[i][1] = 0;
-            max_min[i][2] = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
         }
         for(int i = 1; i <= n; i++){
-            max_min[i][0] = Math.min(max_min[i-1][0], max_min[i-1][1]) + arr[i][0];
-            max_min[i][1] = Math.min(max_min[i-1][0], Math.min(max_min[i-1][1], max_min[i-1][2])) + arr[i][1];
-            max_min[i][2] = Math.min(max_min[i-1][1], max_min[i-1][2]) + arr[i][2];
+            maxx[i][0] = Math.max(maxx[i-1][0], maxx[i-1][1]) + arr[i][0];
+            maxx[i][1] = Math.max(maxx[i-1][0], Math.max(maxx[i-1][1], maxx[i-1][2])) + arr[i][1];
+            maxx[i][2] = Math.max(maxx[i-1][1], maxx[i-1][2]) + arr[i][2];
+            minn[i][0] = Math.min(minn[i-1][0], minn[i-1][1]) + arr[i][0];
+            minn[i][1] = Math.min(minn[i-1][0], Math.min(minn[i-1][1], minn[i-1][2])) + arr[i][1];
+            minn[i][2] = Math.min(minn[i-1][1], minn[i-1][2]) + arr[i][2];
         }
-        min = Math.min(max_min[n][0], Math.min(max_min[n][1], max_min[n][2]));
+        max = Math.max(maxx[n][0], Math.max(maxx[n][1], maxx[n][2]));
+        min = Math.min(minn[n][0], Math.min(minn[n][1], minn[n][2]));
 
         System.out.println(max + " " + min);
     }
