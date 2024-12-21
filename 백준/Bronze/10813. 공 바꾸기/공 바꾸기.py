d14b283b647b52ebc90feb9162db8f72023ab523
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2024-12-21 19:01:06 +0900

    [Bronze II] Title: 공 바꾸기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -6,8 +6,6 @@ n, m = map(int, inp().split())
 arr = [i+1 for i in range(n)]
 for i in range(m):
     a,b = map(int, inp().split())
-    tmp = arr[a-1]
-    arr[a-1] = arr[b-1]
-    arr[b-1] = tmp
+    arr[a-1], arr[b-1] = arr[b-1], arr[a-1]
 
 print(" ".join(map(str, arr)))
