21839b9a8ba460e09c1757dd047d0395eb65e6cb
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-03 15:37:36 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 320 ms, Memory: 54892 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,19 @@
+import sys
+inp = sys.stdin.readline
+
+n, m = map(int, inp().strip().split())
+
+# m보다 짧으면 안외움
+dict = {}
+for i in range(n):
+    sttr = inp().strip()
+    if len(sttr) < m: continue
+    dict[sttr] = dict.get(sttr, 0) + 1
+
+# 자주 + 긴거 + 사전순
+# cnt(x), -len(x), x
+
+llist = sorted(dict, key=lambda x: (-dict[x], -len(x), x))
+
+for name in llist:
+    print(name)
