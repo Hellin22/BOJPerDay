e2d07c8ee7718e9e326460d8c194a004e6039c07
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-08 14:55:50 +0900

    [Silver I] Title: 나이트의 이동, Time: 3192 ms, Memory: 34968 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+inp = sys.stdin.readline
+
+tc = int(inp())
+dx, dy = [-2, -1, 1, 2, 2, 1, -1, -2],[1, 2, 2, 1, -1, -2, -2, -1]
+for case in range(tc):
+    n = int(inp())
+    res = 0
+    curx, cury = map(int, inp().split())
+    targetx, targety = map(int, inp().split())
+    visit = [[False] * n for _ in range(n)]
+    q = deque()
+    q.append([curx, cury, 0])
+    visit[curx][cury] = True
+    while q:
+        curs = q.popleft()
+        if curs[0] == targetx and curs[1] == targety: 
+            res = curs[2]
+            break
+        for i in range(8):
+            nx, ny = dx[i] + curs[0], dy[i] + curs[1]
+            if nx < 0 or ny < 0 or nx >= n or ny >= n or visit[nx][ny]: continue
+            visit[nx][ny] = True
+            q.append([nx, ny, curs[2]+1])
+
+    print(res)
