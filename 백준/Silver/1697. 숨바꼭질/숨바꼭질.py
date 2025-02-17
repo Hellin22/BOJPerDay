180aba8969bc54aea555a03860aa9c7166ccfe90
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-17 16:09:23 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 51388 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+
+inp = sys.stdin.readline
+
+n, k = map(int, inp().strip().split())
+if n == k:
+    print(0)
+    exit()
+q = deque()
+q.append([n, 0])
+st = {n}
+def bfs():
+    global k
+    while q:
+        curs = q.popleft()
+        for nx in (curs[0]-1, curs[0]+1, curs[0]*2):
+            if nx == k: return curs[1]+1
+
+            if 0 <= nx and  nx <= 200001 and nx not in st:
+                q.append([nx, curs[1]+1])
+                st.add(nx)
+print(bfs())
