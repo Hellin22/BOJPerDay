36f91943373087997195a13c36825ea7c397784a
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-17 16:46:14 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+
+inp = sys.stdin.readline
+t = int(inp().strip())
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+arr, visit = [], []
+
+
+def bfs(m, n):
+    global arr, visit
+    q = deque()
+    bug = 0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1 and not visit[i][j]:
+                q.append([i, j])
+                bug+=1
+                while q:
+                    curs = q.popleft()
+                    for k in range(4):
+                        nx, ny = curs[0] + dx[k], curs[1] + dy[k]
+                        if nx < 0 or ny < 0 or nx >= n or ny >= m or visit[nx][ny] or arr[nx][ny] != 1: continue
+                        visit[nx][ny] = True
+                        q.append([nx, ny])
+    return bug
+
+def initt(m, n, k):
+    global arr, visit
+    arr = [[0] * m for _ in range(n)]
+    visit = [[False] * m for _ in range(n)]
+    for i in range(k):
+        y, x = map(int, inp().strip().split())
+        arr[x][y] = 1
+
+for _ in range(t):
+    m, n, k = map(int, inp().strip().split())
+    initt(m, n, k)
+    print(bfs(m, n))
+
