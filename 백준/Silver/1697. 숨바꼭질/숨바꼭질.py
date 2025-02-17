c7258acd5d9d93b553c05417872e9103633e3a7c
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-17 16:10:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 108 ms, Memory: 44220 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -17,7 +17,7 @@ def bfs():
         for nx in (curs[0]-1, curs[0]+1, curs[0]*2):
             if nx == k: return curs[1]+1
 
-            if 0 <= nx and  nx <= 200001 and nx not in st:
+            if 0 <= nx and  nx <= 100001 and nx not in st:
                 q.append([nx, curs[1]+1])
                 st.add(nx)
 print(bfs())
