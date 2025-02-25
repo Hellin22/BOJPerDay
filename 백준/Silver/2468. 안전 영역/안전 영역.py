0445742cddd0a6f7bb91c4f4bf345a405c85c237
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-25 13:15:35 +0900

    [Silver I] Title: 안전 영역, Time: 1536 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,28 +1,37 @@
 import sys
 from collections import deque
 inp = sys.stdin.readline
-n = int(inp())
-dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
-arr = [list(map(int, inp().split())) for _ in range(n)]
-maxHeight = max(max(row) for row in arr)
-res = 1
-for height in range(1, maxHeight):
-    visit = [[False] * n for _ in range(n)]
-    q = deque()
-    safeZone = 0
-    for i in range(n):
-        for j in range(n):
-            if visit[i][j] == True or arr[i][j] <= height: continue
-            visit[i][j] = True
-            q.append([i, j])
-            safeZone+=1
-            while q:
-                curs = q.popleft()
-                for k in range(4):
-                    nx, ny = curs[0] + dx[k], curs[1] + dy[k]
-                    if nx < 0 or ny < 0 or nx >= n or ny >= n or visit[nx][ny] == True or arr[nx][ny] <= height: continue
-                    visit[nx][ny] = True
-                    q.append([nx, ny])
-    res = max(res, safeZone)
 
-print(res)
+n = int(inp())
+
+area = [list(map(int, inp().split())) for _ in range(n)]
+
+maxx = max(area[0])
+minn = min(area[0])
+for i in range(1, len(area)):
+    maxx = max(maxx, max(area[i]))
+    minn = min(minn, min(area[i]))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+max_cnt = -1
+for i in range(max(0, minn-1), maxx+1): # 2~9 -> 비의양임. 비가 2오면 2인 지점은 잠김
+    cnt = 0
+    queue = deque()
+    visit = [[False]*n for _ in range(n)]
+    for j in range(n):
+        for k in range(n):
+            if area[j][k] > i and not visit[j][k]:
+                visit[j][k] = True
+                queue.append((j, k))
+                while queue:
+                    cur_x, cur_y = queue.popleft()
+                    for l in range(4):
+                        nx = cur_x + dx[l]
+                        ny = cur_y + dy[l]
+                        if nx < 0 or ny < 0 or nx >= n or ny >= n or visit[nx][ny] or area[nx][ny] <= i: continue
+                        visit[nx][ny] = True
+                        queue.append((nx, ny))
+                cnt += 1
+                max_cnt = max(cnt, max_cnt)
+print(max_cnt)
