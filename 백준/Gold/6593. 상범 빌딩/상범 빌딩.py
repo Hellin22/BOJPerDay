7f7c2a28e3114885b0845d4f3a225351222b0c85
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-25 20:33:54 +0900

    [Gold V] Title: 상범 빌딩, Time: 92 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -1,46 +1,44 @@
 import sys
 from collections import deque
 inp = sys.stdin.readline
+directions = [(0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1), (1, 0, 0), (-1, 0, 0)]# dz, dx, dy
+# 4*5 -> 3번
 
-dx, dy, dz = [0, 1, 0, -1, 0, 0],[1, 0, -1, 0, 0, 0],[0, 0, 0, 0, 1, -1]
+def bfs(s_z, s_x, s_y):
+    global n, m, h
+    visit = [[[0] * m for _ in range(n)] for _ in range(h)]
+    dq = deque()
+    dq.append((s_z, s_x, s_y, 0))
+    visit[s_z][s_x][s_y] = 1
+    while dq:
+        z, x, y, cnt = dq.popleft()
+        for dz, dx, dy in directions:
+            nz, nx, ny = z + dz, x+dx, y+dy
+            if nz < 0 or nx < 0 or ny < 0 or nz >= h or nx >=n or ny >= m or visit[nz][nx][ny] == 1 or arr[nz][nx][ny] == "#": continue
+            if arr[nz][nx][ny] == "E":
+                return cnt+1
+            visit[nz][nx][ny] = 1
+            dq.append((nz, nx, ny, cnt+1))
+
 
 while True:
-    l, n, m = map(int, inp().strip().split())
-    if (l, n, m) == (0, 0, 0): break
+    h, n, m = map(int, inp().strip().split())
+    if (h, n, m) == (0, 0, 0): break
     arr = []
-    for k in range(l):
-        floor = []
-        for i in range(n):
-            strr = list(inp().strip())
-            floor.append(strr)
-        arr.append(floor)
-        inp().strip()
-    sx, sy, sz, ex, ey, ez = 0, 0, 0, 0, 0, 0
-    for k in range(l):
-        for i in range(n):
-            for j in range(m):
-                if arr[k][i][j] == "S":
-                    sz, sx, sy = k, i, j
-                if arr[k][i][j] == "E":
-                    ez, ex, ey = k, i, j
-    
-    q = deque()
-    q.append([sz, sx, sy])
-    arr[sz][sx][sy] = 0
+    s_x, s_y, s_z = 0, 0, 0
+    # e_x, e_y, e_z = 0, 0, 0
     flag = False
-    while q:
-        z, x, y = q.popleft()
-        for i in range(6):
-            nz, nx, ny = z+dz[i], x+dx[i], y+dy[i]
-            if nz < 0 or nx < 0 or ny < 0 or nz >= l or nx >= n or ny >= m: continue
-            if arr[nz][nx][ny] == "E": 
-                print(f"Escaped in {arr[z][x][y]+1} minute(s).")
-                q.clear()
-                flag=True
-                break
-            if arr[nz][nx][ny] != ".": continue
-
-            arr[nz][nx][ny] = arr[z][x][y]+1
-            q.append([nz, nx, ny])
-    if not flag:
-        print("Trapped!")
+    for height in range(h):
+        sttr = []
+        for x in range(n):
+            sttr.append(inp().strip())
+            if not flag:
+                for y in range(m):
+                    if sttr[x][y] == "S":
+                        s_z, s_x, s_y = height, x, y
+                        flag = True
+                        break
+        arr.append(sttr)
+        inp()
+    res = bfs(s_z, s_x, s_y)
+    print("Trapped!" if res is None else f"Escaped in {res} minute(s).")
