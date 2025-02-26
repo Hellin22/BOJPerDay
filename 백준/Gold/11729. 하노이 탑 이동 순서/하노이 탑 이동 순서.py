69f28e5b131784550d06c7dd8bc135fa69cdbef8
Hellin22 <95176614+Hellin22@users.noreply.github.com>
2025-02-26 13:50:43 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 816 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -4,14 +4,17 @@ n = int(inp().strip())
 llist = []
 def hanoi(n, frrom, to, by):
     if n == 1:
-        llist.append([frrom, to])
+        print(frrom, to)
+        # llist.append([frrom, to])
         return
     
     hanoi(n-1, frrom, by, to)
-    hanoi(1, frrom, to, by)
+    print(frrom, to)
+    # hanoi(1, frrom, to, by)
     hanoi(n-1, by, to, frrom)
 
+# print(len(llist))
+print(2**n-1)
 hanoi(n, 1, 3, 2)
-print(len(llist))
-for i in range(len(llist)):
-    print(llist[i][0], llist[i][1])
+# for i in range(len(llist)):
+    # print(llist[i][0], llist[i][1])
