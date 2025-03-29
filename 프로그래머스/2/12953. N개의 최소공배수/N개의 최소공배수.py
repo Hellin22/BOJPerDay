7012e7fa27b12fef7b9bf6e28638291f9ffc013f
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-03-29 19:50:31 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,29 @@
+'''
+lcm 찾는 문제
+lcm은 모든 곱 / gcd
+gcd -> 유클리드 호제법
+gcd(a, b) = gcd(b, r(a%b)) -> r이 0이 될때까지
+
+n개를 gcd하면?
+모든거를 gcd해서 나온거중 최소값?
+
+
+'''
+
+def solution(arr):
+    
+    result = arr[0]
+    for i in range(1, len(arr)):
+        result = lcm(result, arr[i])
+    
+    return result
+
+def gcd_def(a, b):
+    if b == 0:
+        return a
+    if b != 0:
+        return gcd_def(b, a%b)
+
+def lcm(a, b):
+    return (a*b) // gcd_def(a, b)
+
