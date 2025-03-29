565f0b2a4fe7ad2c69094175615174715441a5a2
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-03-29 20:34:36 +0900

    [level 2] Title: 할인 행사, Time: 49.17 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,40 @@
+'''
+윈도우 슬라이싱
+10일간 -> left는 0, right도 처음에 0 -> right 하나씩 늘려가면서 9까지
+정확히 일치하면 끝낸다고함.
+'''
+def solution(want, number, discount):
+    left, right = 0, 9
+    res = 0
+    dt = dict()
+    for i, pwant in enumerate(want):
+        dt[pwant] = number[i]
+    
+    for i in range(10):
+        if discount[i] in dt:
+            dt[discount[i]]-=1
+    flag = True
+    for val in dt.values():
+        if val != 0:
+            flag = False
+            break
+    if flag:
+        res+=1
+    
+    while right != len(discount)-1:
+        if discount[left] in dt:
+            dt[discount[left]]+=1
+        left+=1
+        right+=1
+        if discount[right] in dt:   
+            dt[discount[right]]-=1  
+        
+        flag = True
+        for val in dt.values():
+            if val != 0:
+                flag = False
+                break
+        if flag:
+            res+=1
+            
+    return res
