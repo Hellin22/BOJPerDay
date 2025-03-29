49fd3d38b145f8536bd9b9f3baf3aa519d2653cb
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-03-29 20:16:44 +0900

    [level 2] Title: 괄호 회전하기, Time: 231.34 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,35 @@
+def solution(s):
+    answer = 0
+    stck = []
+    cur = 0
+    for _ in range(len(s)):
+        stck.clear()
+        flag = True
+        for i in range(len(s)):
+            if s[(cur+i) % len(s)] in "[({":
+                stck.append(s[(cur+i) % len(s)])
+            elif s[(cur+i) % len(s)] == "]":
+                if stck and stck[-1] == "[":
+                    stck.pop()
+                else: 
+                    flag = False
+                    break
+            elif s[(cur+i) % len(s)] == ")":
+                if stck and stck[-1] == "(":
+                    stck.pop()
+                else: 
+                    flag = False
+                    break
+            elif s[(cur+i) % len(s)] == "}":
+                if stck and stck[-1] == "{":
+                    stck.pop()
+                else: 
+                    flag = False
+                    break
+        if flag and not stck: 
+            answer+=1
+        cur+=1
+            
+            
+            
+    return answer
