47356c56f564bb4a4a206abe833646782a77bb49
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-03-31 16:52:26 +0900

    [level 2] Title: 땅따먹기, Time: 268.10 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -9,8 +9,6 @@ def solution(land):
         dp[0][j] = land[0][j]
     
     for i in range(1, len(dp)):
-        dp[i][0] = max(dp[i-1][1], dp[i-1][2], dp[i-1][3]) + land[i][0]
-        dp[i][1] = max(dp[i-1][0], dp[i-1][2], dp[i-1][3]) + land[i][1]
-        dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][3]) + land[i][2]
-        dp[i][3] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + land[i][3]
-    return max(dp[len(land)-1][0], dp[len(land)-1][1], dp[len(land)-1][2], dp[len(land)-1][3])
+        for j in range(4):
+            dp[i][j] = max(dp[i-1][:j]+ dp[i-1][j+1:]) + land[i][j]
+    return max(dp[len(land)-1])
