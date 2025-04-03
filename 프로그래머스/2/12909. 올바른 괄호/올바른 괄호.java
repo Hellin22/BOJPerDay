5280b378ab96f6eec857a5dd20639038e390350a
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-04-03 16:12:08 +0900

    [level 2] Title: 올바른 괄호, Time: 15.83 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,26 +3,19 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-
-        System.out.println("Hello Java");
-
         Stack<Character> stck = new Stack<>();
-        for(int i = 0; i <s.length(); i++) {
-            if(s.charAt(i) == '('){
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
                 stck.add('(');
             }
-            else if (s.charAt(i) == ')') {
-                if (stck.empty()) {
-                    return false;
-                } else {
+            else{ // ')'
+                if(stck.empty()) return false;
+                else{
                     stck.pop();
                 }
             }
         }
-
-        if (!stck.empty()){
-            return false;
-        }
+        if (!stck.empty()) return false;
         
         return answer;
     }
