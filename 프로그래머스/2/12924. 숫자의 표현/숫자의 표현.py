257f7fcf15ede5639a6f3a955416a934b685c104
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-04-11 20:08:38 +0900

    [level 2] Title: 숫자의 표현, Time: 1.45 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,28 @@
+'''
+투포인터
+left, right = 0
+
+'''
+
+def solution(n):
+    if n == 1: return 1
+    answer = 1
+    left, right = 1, 1
+    summ = left
+    while True:
+        if summ == n:
+            answer+=1
+            right+=1
+            summ+=right
+        elif summ < n:
+            right+=1
+            if right > n: break
+            summ+=right
+        else: 
+            summ-=left
+            left+=1
+            
+        if left >= right: break
+        
+        
+    return answer
