7e5765c7d83505b15f739bafbb30825c49424f86
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-04-19 15:45:45 +0900

    [level 3] Title: 기지국 설치, Time: 2.26 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,25 +1,16 @@
-from collections import deque
 import math
 def solution(n, stations, w):
     answer = 0
     
-    # n은 2억 -> 모두 다 보려고하면 안됨.
-    # w가 1이라면? 
-    stations = deque(stations) # deque로 변경
-    
     cur = 1
-
-    while cur <= n:
-        if not stations:
-            if n-cur == 0: answer+=1
-            else: answer += math.ceil((n-cur)/(2*w+1))
-            break
-        if stations[0]-w > cur:
-            answer+=1
-            cur = cur+2*w+1
-        else: 
-            cur = stations[0]+w+1
-            stations.popleft()
-
-    
+    for idx in stations:
+        if idx-w-1 >= cur:
+            answer += math.ceil((idx-w-cur) / (2*w+1))
+            
+        cur = idx+w+1
+        
+    if cur <= n:
+        answer+=math.ceil((n-cur+1)/(2*w+1))
+        
+        
     return answer
