98e68d2419aaf9bc93a1223aa7c0c6e67f8fa8ff
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-05-07 17:00:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.89 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    z_cnt = 0
+    cnt = 0
+    # 1. 모든 0 제거하기
+    # 2. x의 길이 c를 다시 2진법으로 바꾸기
+    # 3. 0의 개수 + 몇번 반복했는지 저장
+    
+    while s != "1":
+        z_cnt += s.count("0")
+        s = s.replace("0", "")
+        s = bin(len(s))[2:]
+        cnt+=1        
+    return [cnt, z_cnt]
