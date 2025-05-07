849066ac850c91442c0b52c04c3fa5d504ba4e02
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-05-07 16:55:57 +0900

    [level 2] Title: 짝지어 제거하기, Time: 94.84 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = 0
+
+    stck = []
+    # 결국에 2개씩 가면됨.
+    for i in s:
+        if stck and stck[-1] == i:
+            stck.pop()
+        else: stck.append(i)
+    
+    return answer if stck else 1
