5be8e66a6566d3f50abf4235a65c3597608ba9d6
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-06-21 23:05:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -4,6 +4,6 @@ def solution(n,a,b):
         a = a//2 if a % 2 == 0 else a//2+1
         b = b//2 if b % 2 == 0 else b//2+1
         if a == b:
-            return answer+1
+            return answer+1 #
         answer+=1
 
