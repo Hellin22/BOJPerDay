1cd35abeefa4e94355e780d3bb4b103ff15a0a0c
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-06-28 14:08:58 +0900

    [level 3] Title: 풍선 터트리기, Time: 150.95 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,23 +1,27 @@
-'''
-최후에 남길 수 있는 풍선 구하기
-'''
 
-
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
 def solution(a):
-    answer = 2
-    n = len(a)
-    if n == 1: return 1
-    elif n == 2: return 2
-
-    left, right = [1000000001] * n, [1000000001] * n
-    
-    for i in range(1, n-1):
-        left[i] = min(left[i-1], a[i-1])
-    
-    for i in range(n-2, 0, -1):
-        right[i] = min(right[i+1], a[i+1])
-    
-    for i in range(1, n-1):
-        # left[i]는 i번째 일때 왼쪽의 max, right[i]는 i번째 일때 오른쪽의 max
-        if max(left[i], right[i], a[i]) != a[i]: answer+=1
+    answer = 1
+    M = min(a)
+    for _ in range(2):
+        m = a[0]
+        i = 1
+        while m != M:
+            if m >= a[i]:
+                m = a[i]
+                answer += 1
+            i += 1
+        a.reverse()
     return answer
