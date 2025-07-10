4c0ca2d0fb1ef0dff91eabdec4707352e5f40174
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-07-10 16:42:25 +0900

    [level 2] Title: 배달, Time: 2.94 ms, Memory: 9.78 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,56 +1,35 @@
-'''
-1번 마을에서 각 마을로 음식 배달
-k시간 이하로 가능한 마을에서만 주문 받을거임.
-다익스트라를 쓸꺼임.
-
-'''
-import heapq
+from heapq import heappush, heappop
 
 def solution(n, road, K):
-    answer = 0
-    INF = float('inf')
+    # 마을개수 n, 도로 road, k시간
+    # 1번마을에서 시작
     
-    arr = [[] for _ in range(n+1)]
-    dist_list = [INF] * (n+1)
-    dist_list[1] = 0
-    visit = [0] * (n+1)
-    
-    for frrom, tto, dist in road:
-        arr[frrom].append((tto, dist)) # 도착정점, 거리
-        arr[tto].append((frrom, dist)) # 양방향 간선이라 2개 다 넣음
-    # 1번에서 시작해야함. -> 1번에서 갈 수 있는곳을 전부 넣어줌.
-    # 1. 시작정점 추가
-    hq = []
-    
-    for tto, dist in arr[1]:
-        if 0 + dist <= K:
-            dist_list[tto] = min(dist_list[tto], 0 + dist)
-            heapq.heappush(hq, (0+dist, tto))
-    while hq: # hq가 빌때까지 진행
-        dist_one, node = heapq.heappop(hq)
+    graph = [[] for _ in range(n+1)]
+    for a, b, dist in road:
+        graph[a].append((dist, b))
+        graph[b].append((dist, a))
         
-        # node에서 tto로 가야함. -> dist[tto]가 더 작을때만 감
-        for tto, dist_two in arr[node]:
-            if dist_one + dist_two <= dist_list[tto]:
-                if dist_one + dist_two <= K: # k시간보다 작을때만 감.
-                    heapq.heappush(hq, (dist_one + dist_two, tto))
-                    dist_list[tto] = dist_one + dist_two
-                    
-    for di in dist_list:
-        if di != INF:
+    hq = []
+    dst = [10000000000000] * (n+1)
+    dst[1] = 0 # 1번마을은 dist 0
+    heappush(hq, (0, 1))
+    
+    while hq:
+        dist, node = heappop(hq)    
+        
+        if dist > dst[node]: continue
+        
+        for d, a in graph[node]:
+            if dist + d > dst[a]: continue
+            else:
+                heappush(hq, (dist+d, a))
+                dst[a] = dist+d
+    
+    print(dst)
+    answer = 0
+    for di in dst:
+        if di <= K:
             answer+=1
     return answer
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    
