d6993263d1e0b29b2de619b102c2a4e754b95390
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-06 14:47:52 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,15 +1,20 @@
 /*
-입양을 간 동물 중, 보호 기간이 가장 길었던 동물 2마리의 아이디와 이름 조회
--> 입양을 무조건 갔어야함.
--> 보호 기간이 가장 길었던 것 중 2마리 limit 2
-즉, in에도 있고 out에도 있는것.
-inner join 쓰기 -> ANIMAL_OUTS 기준으로 left outer join 해도 될듯
-IN의 DATETIME하고 OUT의 DATETIME을 비교해서 가장 긴 친구 2마리
+ANIMAL_INS
+ANIMAL_OUTS
+
+입양 간 동물 중 보호기간 가장 길었던 동물 2마리 (아이디, 이름)
+
+OUTS에 있는 것들. -> RIGHT JOIN (INNER JOIN도 됨)
+DATETIME 차이가 가장 큰것.
+
+-> OUTS에 있는데 INS에 없는거도 있음. -> INNER JOIN
+
 */
 
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_INS AS A
-INNER JOIN ANIMAL_OUTS AS B
-ON A.ANIMAL_ID = B.ANIMAL_ID
-ORDER BY DATEDIFF(A.DATETIME, B.DATETIME) ASC
+
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_OUTS AS O
+JOIN ANIMAL_INS AS I
+ON O.ANIMAL_ID = I.ANIMAL_ID
+ORDER BY (O.DATETIME - I.DATETIME) DESC
 LIMIT 2
