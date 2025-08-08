dde98d271e6e97e0956404716e04e915468bc2ee
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-08 16:17:07 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
 # 7월 아이스크림 총 주문량과 상반기 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개
 # TOTAL_ORDER의 합 큰거 3개?
-# GROUP BY로 이름 묶기 -> TOTAL_ORDER의 합을 구해야함.
+# GROUP BY로 이름 묶기 -> TOTAL_ORDER의 합을 구해야함
 
 SELECT A.FLAVOR
 FROM FIRST_HALF A
@@ -11,4 +11,3 @@ JOIN (SELECT FLAVOR, SUM(TOTAL_ORDER) AS JULY_ORDER
     ON A.FLAVOR = B.FLAVOR
 ORDER BY (A.TOTAL_ORDER + B.JULY_ORDER) DESC
 LIMIT 3
-
