ffc42e0f79b23af33cd28f46668b23995feaa7c2
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-12 14:21:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,4 +1,10 @@
+'''
+문자열 s -> 구분된 숫자들
+
+최소 최대로 구분 (최소) (최대)
+split -> 0, -1
+'''
+
 def solution(k):
-    ss = k.split(" ") # split
-    ss.sort(key = lambda x: int(x))
-    return f"{ss[0]} {ss[-1]}"
+    splited = sorted(list(map(int, k.split(' '))))
+    return f"{splited[0]} {splited[-1]}"
