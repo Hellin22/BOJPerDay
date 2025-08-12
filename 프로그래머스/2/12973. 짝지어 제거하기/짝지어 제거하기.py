59742a88b1a4448a45d849f11e9afed71c39c414
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-12 15:45:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 106.44 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,11 +1,34 @@
 def solution(s):
-    answer = 0
-
     stck = []
-    # 결국에 2개씩
     for i in s:
-        if stck and stck[-1] == i:
-            stck.pop()
-        else: stck.append(i)
+        if not stck: stck.append(i)
+        else: 
+            if stck[-1] == i:
+                stck.pop()
+            else: stck.append(i)
+    return 1 if not stck else 0
     
-    return answer if stck else 1
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
+    소문자 문자열
+    같은 알파벳 2개 짝 -> 삭제 후 이어붙임
+    문자열 모두 제거 -> 종료
+    가능 유무를 1, 0
+    
+    완탐은 안될듯
+    이거 어캐풀지? ㅋㅋ
+    abcddcba
+    3 4
+    abccba
+    
+    left-1, left로 가면 되나?
+    
+    스택?ㅇㄴㅇㅇㅇㅇ
+    '''
