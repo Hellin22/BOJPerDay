cc59c36b2d234b6d57e748d604ad23f638033575
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-13 16:20:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,28 +1,40 @@
 '''
-lcm 찾는 문제
-lcm은 모든 곱 / gcd
-gcd -> 유클리드 호제법
-gcd(a, b) = gcd(b, r(a%b)) -> r이 0이 될때까지
+여러개 수 중에서 최소 공배수 구하기
+계속 반복하면서 최소공배수 구하기?
+최소공배수는?
+a * b / gcd = lcm
 
-n개를 gcd하면?
-모든거를 gcd해서 나온거중 최소값?
+gcd는? a > b
+a / b = c - d
+b / c = k - e
 
+8/6 = 1 - 2
+6 / 2 = 3 - 0
 
+14 / 8 = 1 - 6
+8 / 6 = 1 - 2
+6 / 2 = 3 - 0
+
+e가 0이 될때까지?
 '''
 def solution(arr):
     
-    result = arr[0]
-    for i in range(1, len(arr)):
-        result = lcm(result, arr[i])
+    def gcd(a, b):
+        # 항상 a가 b보다 큼
+        c, d = 111111, 111111
+        while d != 0: # 나머지가 0이 아니면 반복
+            c, d = a // b, a%b
+            if d == 0: return b
+            
+            a, b = b, d
     
-    return result
-
-def gcd_def(a, b):
-    if b == 0:
-        return a
-    if b != 0:
-        return gcd_def(b, a%b)
-
-def lcm(a, b):
-    return (a*b) // gcd_def(a, b)
-
+    
+    arr.sort(key = lambda x: -x)
+    gcd_num = arr[0]
+    ans = arr[0]
+    for i in arr[1:]:
+        gcd_num = gcd(ans, i)
+        ans *= i
+        ans//=gcd_num
+    
+    return ans
