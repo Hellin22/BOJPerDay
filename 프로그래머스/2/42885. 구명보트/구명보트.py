d1a53778adf853a0f20c387c4da39ff5dee82eaf
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-13 18:11:26 +0900

    [level 2] Title: 구명보트, Time: 18.19 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,18 +1,26 @@
+from collections import deque
 def solution(people, limit):
-    answer = 0    
-    people.sort()
-    right = len(people)-1
-    left = 0
     
-    while True:
-        if left > right: break
-        cur = 0
-        cur += people[right]
-        right-=1
-        answer+=1
-        if limit >= cur+people[left]: # 2명이 최대
-             left+=1
+    dq = deque(sorted(people, key=lambda x: -x))
+    ans = 0
+    while dq:
+        if len(dq) >= 2 and dq[0] + dq[-1] <= limit:
+            dq.popleft()
+            dq.pop()
+        else: dq.popleft()
+        ans+=1
+    return ans
+    '''
+    한번에 최대 2명 + 무게제한
+    
+    보트 최대한 적게 사용하기
+    
+    최소 최대를 하나씩 태워보기?
+    
+    1 3 5 7 9 -> 최대 이다?
+    
+    2명이니까 최대 최소끼리 태우기?
+    
+    '''
     
     
-    
-    return answer
