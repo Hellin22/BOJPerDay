1ba0c2fa028580fb6fc72e4e1816c19113fab33e
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-13 15:43:52 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,20 +1,26 @@
-'''
-n은 1~10억
-k는 1이상 자연수  순간이동은 건전지x, k만큼 이동하려면 건전지 k만큼 사용 / 0부터 n까지 가야함
-1. 현재 이동거리 x2 만큼 이동가능
-
-'''
 from collections import deque
+
 def solution(n):
-    ans = 0
     
+    dq = deque()
+    # 반대로 n에서 2분의 1씩 줄이기?
+    dq.append((n, 0))
+    ans = 0
     while n != 0:
-        if n % 2 == 0: # 2로 나누기
+        if n % 2 == 0: # n이 2로 나눠지면
             n//=2
-        else:
-            n-=1
+        else: 
             ans+=1
+            n-=1
     
     return ans
+    '''
+    K칸 점프, *2 이동
+    이동은 건전지 안들고 점프는 K만큼 듦
+    N까지 가려고함
+    건전지 사용량 제일 적은 방법
+    처음 위치는 0부터 시작
     
-        
+    + 되는거만 존재 -> N을 벗어나면 추가 X
+    BFS는 DEQUE로
+    '''
