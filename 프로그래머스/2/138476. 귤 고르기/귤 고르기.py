aa3a801f3d3adc94c22bc8496a0b28f8eea4d8ed
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-13 15:57:25 +0900

    [level 2] Title: 귤 고르기, Time: 341.94 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,13 +1,21 @@
-from collections import Counter
-
+from heapq import heappush, heappop
 def solution(k, tangerine):
-    answer = 0
     
-    a = Counter(tangerine)
-    
-    a = list(a.values())
-    a.sort(reverse = True)
-    for i, val in enumerate(a):
-        answer+=val
-        if answer >= k:#
-            return i+1
+    hq = []
+    a = [0] * 10000001
+    for i in tangerine:
+        a[i]+=1
+    for i in a:
+        if i != 0:
+            heappush(hq, -i)
+    ans = 0
+    while hq and k > 0:
+        
+        k-= -heappop(hq)
+        ans+=1
+    return ans        
+    '''
+    k개를 골라 상자 하나에 담아 판매
+    크기별로 분류 -> 크기가 가장 작은걸로?
+    heap?
+    '''
