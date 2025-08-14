60a1b70496ca1b1a335aee1b190508fc0bc19724
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-14 17:51:00 +0900

    [level 2] Title: [1차] 캐시, Time: 120.58 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,47 +1,45 @@
-'''
-cacheSize == 0~3 숫자임
-cities는 도시의 이름 == "Jeju"  100,000개
-(대소문자 구분 없음 -> 대소문자가 바껴서 들어온다는 의미? 맞네)
--> cities에는 lower 추가해주기
-'''
+
 def solution(cacheSize, cities):
-    answer = 0
+    
+    '''
+    캐시 크기 얼마로 설정
+    
+    배열 순서대로 처리 -> 총 실행시간
+    hit = 1
+    miss = 5
+    
+    dict 사용?
+    if hit면 ok
+    elif miss면 
+    
+    '''
+    if cacheSize == 0: return 5*len(cities)
+    for i,ct in enumerate(cities):
+        cities[i] = ct.upper()
     cache = dict()
-    curTime = 0
-    for i in range(len(cities)):
-        curTime+=1
-        sttr = cities[i].lower()
-        if sttr in cache:
-            # 있는 경우
-            answer+=1
-            cache[sttr] = curTime
-        else:
-            # 없는 경우
-            answer+=5
-            if cacheSize == 0: continue
-            # dict의 크기는 3을 넘어가면 안됨
-            if len(cache) < cacheSize:
-                cache[sttr] = curTime
-            else:
-                delKey = ""
-                delVal = 1000000
-                for key, val in cache.items():
-                    if delVal > val:
-                        delKey, delVal = key, val
-                # print(delKey)
-                del cache[delKey]
+    ans = 0
+    for idx, ct in enumerate(cities):
+        if len(cache) < cacheSize: # 캐시 용량 남아있으면
+            if ct not in cache: # 만약 캐시에 존재안하면
+                ans += 5 # miss기 때문
+            else: #이미 캐시에 존재하면?
+                ans+=1
                 
-                cache[sttr] = curTime
-        
-    
-    # 여기서 중요한것 -> 현재 캐시에 있는것이 무엇인지 + 캐시에서 빼야할게 무엇인지
-    # dict로 진행 -> if dict에 없다 -> cachesize는 최대 3이니까 다 돌아봐도 됨
-    # dict로 진행 -> if dict에 있다 -> 값 바꿔주기(ans+=1)
-    # elif dict에 없다 -> dict를 돌면서 value가 가장 낮은거를 빼고 현재꺼 추가하기 (ans+=5)
-    # 제일 처음에는 없으니까 5추가해야함
-    # 현재 시간도 저장해놔야함. -> 0으로 시작하자.
-    
-    
-    
-    
-    return answer
+            cache[ct] = idx # 캐시 데이터 갱신
+            
+        else: # 캐시 용량이 없음
+            if ct not in cache: # 캐시에 없다면
+                ans+=5 # miss
+                del_k = "dfw"
+                min_v = 9999999999
+                for k, v in cache.items():
+                    if v < min_v:
+                        min_v = v
+                        del_k = k
+                del cache[del_k] # LRU 작동
+                
+            else: # 캐시에 이미 있다면
+                ans+=1
+                
+            cache[ct] = idx
+    return ans
