9c66d2f6782383e33a3dfed9d02ca94bfaddd64a
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-16 15:03:16 +0900

    [level 2] Title: 프로세스, Time: 0.28 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,36 +1,32 @@
-'''
-결국 우선순위가 가장 높은건지를 알고 있어야함.
-우선순위는 최대 9 -> dt에 저장하면 되겠다. (list보다 좋을듯)
-
-'''
-
-import heapq
+from heapq import heappush, heappop
 from collections import deque
 def solution(priorities, location):
-
-    cnt = 0
-    hq = []
-    priorities = deque(priorities)
-    for pri in priorities:
-        heapq.heappush(hq, -pri)
-        
-    for i, pri in enumerate(priorities):
-        if location == i:
-            priorities[i] = [priorities[i], 1]
-        else: 
-            priorities[i] = [priorities[i], 0]
     
-    while priorities:
-        if priorities[0][0] == -hq[0]:
-            if priorities[0][1] == 1:
-                return cnt+1
-            else: 
-                cnt+=1
-                priorities.popleft()
-                heapq.heappop(hq)
-        else:
-            a = priorities.popleft()
-            priorities.append(a)
+    # 어떤 놈을 타겟하는지 설정. -> 타켓하는놈은 1로 아닌놈들은 -1로
+    # heap으로 처리
+    hq = []
+    for i in priorities:
+        heappush(hq, -i)
+    for i in range(len(priorities)):
+        if i != location: priorities[i] = [priorities[i], -1]
+        else: priorities[i] = [priorities[i], 1]
         
+    dq = deque(priorities)
+    while dq:
+        a, b = dq.popleft() # 현재 프로세스 꺼내기
+        # 우선순위가 제일 높은지 확인
+        if a == -hq[0]:
+            if b == 1: return len(priorities) - len(dq)
+            else: heappop(hq)
         
-    return answer
+        else: # 우선순위가 제일 안높음
+            dq.append([a, b])
+    
+    '''
+    1. 큐에서 대시 프로 하나 꺼냄
+    2. 큐에 꺼낸거보다 우선순위 높은거 있으면 다시 큐에 넣음
+    3. 없다면? 방금꺼 실행 -> 이 프로세스는 종료
+    
+    '''
+    
+    
