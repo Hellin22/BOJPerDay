d093463ea489f31f20fb5389b7c3b119f69a011d
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-21 14:55:50 +0900

    [level 2] Title: 모음 사전, Time: 1.92 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,32 +1,40 @@
-# dfs로 풀어야할듯?
-'''
-# 전역
-str= ""
-alphabets = ["A", "E", "I", "O", "U"]
-dt = dict()
-def 함수이름:
-    for i in range(5):
-        str+=alphabets[i]
-        dt[str] = len(dt)+1      
-        함수()
-        str = str[0:len(str)-1] # 젤 뒤에꺼 빼기
-'''
-sttr = ""
-alphabets = ["A", "E", "I", "O", "U"]
-dt = dict()
-
-def dfs():
-    global sttr
-    if len(sttr) == 5:
-        return
-    
-    for i in range(5):
-        sttr+=alphabets[i]
-        dt[sttr] = len(dt)+1
-        dfs()
-        sttr = sttr[0:len(sttr)-1]
 
 def solution(word):
-    answer = 0
+    '''
+    A E I O U
+    
+    A AA AAA AAAA AAAAA
+    AAAAE AAAAI AAAAO AAAAU
+    AAAEA AAAEE ...
+    
+    어떤 단어가 몇번째인지?
+    '''
+    llist = ['A','E','I','O','U']
+    # 0 1 2 3 4
+    # 0
+    # 00 000 0000 00000 -> len(str)이 5이면 더이상 추가 못하니까 종료조건이면 더이상 추가 못하니까 종료조건
+    ans = ""
+    cnt = 0
+    flg = False
+    def dfs():
+        nonlocal word, ans, cnt, flg
+        if ans == word: # 만약에 둘이 같다면
+            flg = True
+            return
+        
+        if flg == True: return
+        
+        cnt+=1
+    
+        # 1. 만약 길이가 5이다? 너 못늘리기 때문에 pop
+        if len (ans) == 5: 
+            return
+        # 2. 만약 길이가 5가 아니다? for문 돌리기 -> 그 안에서 +=하기
+        else:
+            for i in llist:
+                ans = ans + i # ans 갱신하기
+                dfs()
+                ans = ans [:-1] # 반복하고 나서는 제일 뒤에꺼 빼주기
+    
     dfs()
-    return dt[word]
+    return cnt
