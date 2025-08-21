0c118358dd02b89ca661f9d6e224c1dcf7e080ac
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-21 15:35:44 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 789.13 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,40 +1,21 @@
-'''
-numbers에서 numbers[i]보다 뒤에 있으면서 큰 수를 answer.append 하면됨
-만약 없다? -1을 넣기
-len(numbers)는 최대 백만 -> 하나씪 다 보면서 최대값을 보는건 의미 없음
-뒤에서부터 보면서 -> 자신보다 크기만하면 됨. + 가장 가까이에 있어야함
-내림차순배열? -> n2이 됨.
-stack 사용?
-1. -1번째 stck에 집어넣고 -1로 바꿈
-2. 뒤에서부터 오면서 if stck[-1]이 현재보다 크다? 그러면 그거 적용후 자신도 집어넣기
-elif stck[-1]이 현재보다 작거나 같다? 그러면 큰수가 아니니까 top 빼야함.
-언제까지? top이 뒷큰수일때 or stck이 빌때까지
-'''
+
 def solution(numbers):
-    answer = []
     
-    next_bigger_nums = [] # ㅁ마지막에 뒤집기
+    '''
+    자신보다 크면서 가장 가까이에 있는 수 찾기 -> 없으면 -1
+    뒤에서 하기 or 첨부터 하기?
+    9 1 5 3 6 2 
+    
+    '''
+    res = [-1] * len(numbers)
     stck = []
-    stck.append(numbers[-1]) # 원소값 넣기 -> numbers[i]
-    next_bigger_nums.append(-1)
-    
-    for i in range(len(numbers)-2, -1, -1):
-        if stck and stck[-1] > numbers[i]:
-            next_bigger_nums.append(stck[-1])
-            stck.append(numbers[i])
+    for i, val in enumerate(numbers):
+        if not stck or stck[-1][1] >= val:
+            stck.append([i, val])
         else:
-            while True:
-                if not stck: 
-                    # stck이 빈다면 -1임 + 내꺼넣기    
-                    next_bigger_nums.append(-1)
-                    stck.append(numbers[i])
-                    break
-                elif stck:
-                    if stck[-1] <= numbers[i]: # 빼야함.
-                        stck.pop()
-                    elif stck[-1] > numbers[i]: # 이거임.
-                        next_bigger_nums.append(stck[-1])
-                        stck.append(numbers[i])
-                        break
-    return next_bigger_nums[::-1]
-    
+            while stck and stck[-1][1] < val:
+                tmp_i, tmp_val = stck.pop()
+                res[tmp_i] = val
+            stck.append([i, val])
+            
+    return res
