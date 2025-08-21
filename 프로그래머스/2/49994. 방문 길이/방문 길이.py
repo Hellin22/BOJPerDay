f78e57bf0026df040b4c64a7b2708e519d1933f3
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-21 15:22:39 +0900

    [level 2] Title: 방문 길이, Time: 0.38 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,24 +1,27 @@
-'''
-처음 걸어본 길이의 합 -> 그냥 탐색문제
-만약 범위 벗어나면 안함 -> 범위가 얼마? (5, 5)로 구성.
-(5, 5)에서 시작한다고 가정?
-visit = [11][11][4]
-'''
-
 def solution(dirs):
-    answer=0
-    n = 10
-    dt = {"U":(1, 0), "D":(-1, 0), "R":(0, 1), "L":(0, -1)}
-    dt2 = {"U":0, "R":1, "D":2, "L":3}
-    curx, cury = 5, 5
-    visit = [[[0]*4 for _ in range(n+1)] for _ in range(n+1)]
-    for i, val in enumerate(dirs):
-        nx, ny = curx + dt[val][0], cury + dt[val][1]
-        if 0<=nx<=n and 0<=ny<=n:
-            if visit[curx][cury][dt2[val]] != 1: 
-                answer+=1
-                visit[curx][cury][dt2[val]] = 1
-                visit[nx][ny][(dt2[val]+2)%4] = 1
-            # cur -> nx
-            curx, cury = nx, ny
-    return answer
+    
+    '''
+    처음 걸어본 길의 길이 구하기
+    점을 배열로 잡기
+    00 ~ -5-5 55 5-5 
+    11개 크기
+    
+    
+    '''
+    map = [[[0] * 4 for _ in range(11)] for _ in range(11)] # 0은 안간것 -> 4개의 방향이 있음. 고려해주기
+    
+    x, y = 5, 5
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    dt = {'R':0, 'D':1, 'L':2, 'U':3}
+    cnt = 0
+    for i in dirs:
+        dx, dy = directions[dt[i]][0], directions[dt[i]][1]
+        nx, ny = x+dx, y+dy
+        if nx < 0 or ny < 0 or nx >= 11 or ny >= 11: continue
+        if map[x][y][dt[i]] == 0:
+            map[x][y][dt[i]] = 1
+            map[nx][ny][(dt[i] +2)%4]=1
+            cnt+=1
+        x, y = nx, ny
+    
+    return cnt
