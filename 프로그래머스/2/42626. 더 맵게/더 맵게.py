a6eb6498a3178724e25322a8d58d4aab45ba9e06
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-21 16:03:55 +0900

    [level 2] Title: 더 맵게, Time: 1561.00 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,20 @@
-import heapq
+from heapq import heappush, heappop, heapify
 
-def solution(scoville, K):
-    heapq.heapify(scoville)  # 최소 힙으로 변환
-    mix_count = 0
-
-    while scoville:
-        min1 = heapq.heappop(scoville)
-        if min1 >= K:
-            return mix_count
-        if not scoville:  # 더 섞을 음식이 없다면 실패
-            return -1
-        min2 = heapq.heappop(scoville)
-        new_scoville = min1 + (min2 * 2)
-        heapq.heappush(scoville, new_scoville)
-        mix_count += 1
-
-    return -1  # 모든 경우 실패
+def solution(scoville, k):
+    
+    '''
+    맵지 않은거 + 2번째 맵지않은거 *2 
+    
+    모든 음식의 스코빌을 k 이상으로 만들기 -> heap[0]가 k 이상이면 됨
+    '''
+    heapify(scoville)
+    
+    cnt = 0
+    while len(scoville) >= 2 and scoville[0] < k: # hq가 있고 k를 못넘는다면
+        a, b = heappop(scoville), heappop(scoville)
+        heappush(scoville, a+b*2)
+        cnt+=1
+        
+    if not scoville or scoville[0] < k: # hq 없거나 k 이상으로 못만들면
+        return -1
+    else: return cnt
