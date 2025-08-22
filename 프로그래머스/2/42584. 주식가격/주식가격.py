9b4e1f2fdf14352c8c745cc9c6a7a015dacd1ad4
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-22 14:45:53 +0900

    [level 2] Title: 주식가격, Time: 40.52 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -9,13 +9,12 @@ def solution(prices):
     stck = []
     ans = [0] * len(prices)
     for i in range(len(prices)):
-        if not stck: stck.append([prices[i], i]) # stck이 비어있으면 바로 넣기
-        else: # stck이 안비어있으면
-            while stck and stck[-1][0] > prices[i]: # 빼는 과정 -> stck이 존재하고 stck[-1]이 더 크다면 빼기
-                val, idx = stck.pop()        
-                ans[idx] = i-idx
-            stck.append([prices[i], i])
-        
+        while stck and stck[-1][0] > prices[i]:
+            val, idx = stck.pop()        
+            ans[idx] = i-idx
+
+        stck.append([prices[i], i])
+
     for i, val in enumerate(ans):
         if ans[i] == 0: ans[i] = len(ans)-i-1
     return ans
