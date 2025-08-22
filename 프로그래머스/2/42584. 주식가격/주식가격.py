30fe635ec4d47567eddee8afd3153efbfb79116c
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-22 14:43:32 +0900

    [level 2] Title: 주식가격, Time: 36.63 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,34 +1,21 @@
 def solution(prices):
-    # 떨어지지 않은 일수를 생각해야함.
+    
     '''
-    1 2 3 2 3
+    주식 가격이 언제 떨어지는지
+    뒤에서부터 넣어야할거같음
     
-    1
-    1 2
-    1 2 3
-    stck.top하고 비교했을때 3 -> 2는 떨어짐.
-    따라서 3은 (idx하고 같이 저장해야할듯) idx하고 2의 idx 비교해서 1 넣기
-    1 2 2
-    stck.top은 2이고 새로운건 3 -> 3은 마지막이니까 0 넣고
-    1 2 2 3
-    0 1 3 4
-    
-    idx - top.idx
-    쭉쭉쭉 진행 -> 결국 stack 사용하면 될거같음.
     '''
     
     stck = []
-    answer = [0] * len(prices)
-    for i, pri in enumerate(prices):
-        while stck and stck[-1][1] > pri:
-            # stck에 원소가 있는데 감소한 경우
-            idx, price = stck.pop()
-            answer[idx] = i - idx
-            
-                
-        stck.append((i, pri)) # 다 뺐으면 추가
-    
-    for i in range(len(stck)):
-        answer[stck[i][0]] = stck[-1][0] - stck[i][0]
-    
-    return answer
+    ans = [0] * len(prices)
+    for i in range(len(prices)):
+        if not stck: stck.append([prices[i], i]) # stck이 비어있으면 바로 넣기
+        else: # stck이 안비어있으면
+            while stck and stck[-1][0] > prices[i]: # 빼는 과정 -> stck이 존재하고 stck[-1]이 더 크다면 빼기
+                val, idx = stck.pop()        
+                ans[idx] = i-idx
+            stck.append([prices[i], i])
+        
+    for i, val in enumerate(ans):
+        if ans[i] == 0: ans[i] = len(ans)-i-1
+    return ans
