34eb9933907e9129737f4fe9d1f801ab950b0151
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-25 19:18:42 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.91 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,47 +1,31 @@
-'''
-["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
-["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
-영문 대소문자, 숫자, 공백(" "), 마침표("."), 빼기 부호("-")
-1. HEAD 부분을 기준으로 사전 순 (대소문자 구분x)
-2. NUMBER의 숫자 순으로 정렬한다. 9 < 10 < 0011 < 012 < 13 < 014 순으로 정렬 (0 무시됨.)
--> int 적용하면됨
-3.  원래 입력에 주어진 순서를 유지한다.
-
-자 그러면 1. HEAD, NUMBER를 각각 구한다.
-2. HEAD기준(대소문자 구분X -> 모두 소문자로), NUMBER를 (int 적용해서 하기)
-즉, (소문자 HEAD, int(NUMBER), idx) -> idx는 정렬이 바꾸면 안되기 때문임.
-
-'''
-
-
 def solution(files):
-    answer = []
-    heads = []
-    numbers = []
+    '''
+    HEAD / NUMBER / TAIL
+    HEAD는 문자만
+    NUMBER는 숫자(1~5개)
+    TAIL은 나머지
     
-    def headFind(sttr):
-        for i in range(len(sttr)):
-            if 48 <= ord(sttr[i]) <= 57: return i
-    
-    def numberFind(sttr):
-        for i in range(len(sttr)):
-            if 48 <= ord(sttr[i]) <= 57: continue
-            else: return i 
-    
-    for i in range(len(files)): # 모든 파일에 대해서. file = foo9.txt
-        file = files[i] 
+    1. HAED 사전순
+    2. NUMBER 숫자순 (int로)
+    3. 원래 순서
+    '''
+    ans = []
+    hnt = []
+    for idx,file in enumerate(files):
+        h, n = "",""
+        i = 0
+        while file[i].isalpha() or file[i] == " " or file[i] == "." or file[i] == "-":
+            i+=1
         
-        headIdx = headFind(file) # 뭘 넣어줘야할까? 
-        heads.append(file[:headIdx])
-        file = file[headIdx:]
-        numberIdx = numberFind(file) # 48 57
-        numbers.append(int(file[:numberIdx]))
-    headNumberIdx = []
-    
-    for idx in range(len(files)):
-        headNumberIdx.append((heads[idx].lower(), numbers[idx], idx))
-    headNumberIdx.sort()
-
-    for i in range(len(headNumberIdx)):
-        answer.append(files[headNumberIdx[i][2]])
-    return answer
+        h = file[:i]
+        j=i
+        while j < len(file) and file[j].isdigit():
+            if j-i >= 5: break
+            j+=1
+            # 22 23 24 25 26 27
+        n = file[i:j]
+        hnt.append((h, n, idx))
+    hnt.sort(key = lambda x: (x[0].upper(), int(x[1]), x[2]))
+    for i in hnt:
+        ans.append(files[i[2]])
+    return ans
