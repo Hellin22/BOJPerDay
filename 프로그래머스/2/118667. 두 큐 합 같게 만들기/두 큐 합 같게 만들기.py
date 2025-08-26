dd5123a76b68fd7e485e7cfbe958129002174c17
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-26 15:44:07 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 112.16 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,31 +1,31 @@
-'''
-일단 q1, q2의 각각의 합, 모든 합을 구해야함.
-모든 합 절반이 목표치니까 q1, q2중 더 큰값에서 이걸 빼면 될듯?
-종료 -1 조건이 애매했는데, 문제 조건은 길이가 같은 queue1,2임. 
-따라서 전체 합이 홀수가 아니라면 len(q)가 0이 되지 않는 이상 모두 가능한거같음
-'''
 from collections import deque
-def solution(q1, q2):
-    max_cnt = len(q1)
-    q1 = deque(q1)
-    q2 = deque(q2)
-    q1Sum = sum(q1)
-    q2Sum = sum(q2)
-    res_cnt = 0
-    res = q1Sum + q2Sum
-    if res%2 != 0: return -1 # 홀수이면 불가능
-    for i in range(max_cnt*3):
-        if q1Sum == q2Sum:
-            return res_cnt
-        elif q1Sum > q2Sum: # q1에서 빼서 q2에 주기
-            popNum = q1.popleft()
-            q1Sum-=popNum
-            q2.append(popNum)
-            q2Sum+=popNum
-        elif q1Sum < q2Sum: # q2에서 빼서 q1에 주기
-            popNum = q2.popleft()
-            q2Sum-=popNum
-            q1.append(popNum)
-            q1Sum+=popNum
-        res_cnt+=1
-    return -1
+def solution(queue1, queue2):
+    
+    '''
+    작은거에서 큰걸로 원소를 옮기면 됨. 가능할때까지.
+    불가능하다면? -1
+    언제 불가능하지? 
+    cnt가 len+len이 된다면 불가능?
+    '''
+    
+    d1 = deque(queue1)
+    d2 = deque(queue2)
+    
+    cnt = 0
+    d1s = sum(d1)
+    d2s = sum(d2)
+    MAXIMUM = len(d1) + len(d2)*2
+    while cnt <= MAXIMUM and d1s != d2s:
+        if d1s > d2s: # d1s에서 빼고 d2s에 넣기
+            pop_num = d1.popleft()
+            d1s-=pop_num
+            d2s+=pop_num
+            d2.append(pop_num)
+        else:
+            pop_num = d2.popleft()
+            d2s-=pop_num
+            d1s+=pop_num
+            d1.append(pop_num)
+        cnt+=1
+        
+    return cnt if cnt <= MAXIMUM else -1
