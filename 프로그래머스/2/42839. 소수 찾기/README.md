a812218c76bfbea5b9321a5f6e881af68aea5ecd
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-27 13:18:08 +0900

    [level 2] Title: 소수 찾기, Time: 2438.38 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 86.9 MB, 시간: 1921.66 ms
+메모리: 85.5 MB, 시간: 2438.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 04일 13:19:37
+2025년 08월 27일 13:18:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,60 +1,41 @@
-'''
-numbers의 각 자리수를 모두 사용할 수 있는 변수로 적용한다.
-같은 숫자가 있을 수 있다. -> 현재 값은 사용하지 못한다.
-visit 배열을 사용해?
-소수인지 확인할 필요가 있음 -> 최대값을 찾아서 그 최대값 만큼의 배열 선언(2차원)
-최대는 9,999,999이겠지?  천만개의 배열
--> 메모리 초과는 안날거같음. -> 만약 메모리초과가 난다면? 
-9876543가 최대임 -> 7!가지 -> 7!은?
-7 6 5 4 3 2
-42 20 6 -> 120 42 -> 4000(5000) 5백만 * 5000 -> 5,000,000,000 안되.ㅁ 절대 안됨ㅁ.
-
-그러면 이제 어떤걸 사용할지를 정해야함.
--> dfs(몇개를 고를건지, 현재 idx)
-    if 고른 개수가 == cnt라면 
-visit = [0] * len(numbers) -> "17" 이라면 2만큼의 크기임. -> 1 / 7 / 1 7
-dfs(1, 0)
-
-그러면 그냥 itertools에서 
-
-'''
-import itertools
-import math
-
 def solution(numbers):
-    answer = 0
+    '''
+    numbers를 각 1개씩 나누기
     
-    n = len(numbers)
-    # visit = [0] * n
+    1~7의 길이 최대 9999999 천만개
+    '''
+    is_prime = [1] * 10000000
+    is_prime[0] = 0
+    is_prime[1] = 0
+    for i in range(2, 10000000):
+        if is_prime[i] == 1:
+            for j in range(i*i, 10000000, i):
+                is_prime[j] = 0
     
-    # llist = itertools(range(0, n), 1) # 1도 하고 
-    # 어떤 리스트를 파라미터로 줘야하는가? a라는 리스트를 준다고 해보자.
-    a = []
-    for i in range(n):
-        a.append(i)
+    num_list = list(numbers)
+    num = ""
     st = set()
-    for i in range(1, n+1):
-        llist = list(itertools.permutations(a, i))
-        for j in range(len(llist)):
-            sttr = ""
-            # [(0,), (1,), (2,)]
-            # [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
-            for k in range(len(llist[j])):
-                sttr+=str(numbers[llist[j][k]])
-                st.add(int(sttr))
-        
-    maxx = max(st)
-    is_sosu = [0] * 10000000
-    is_sosu[0] = 1
-    is_sosu[1] = 1
-    for i in range(2, int(math.sqrt(maxx))+1):
-        if is_sosu[i] == 1: continue
-        for j in range(i+i, 10000000, i):
-            if is_sosu[j] == 0:
-                is_sosu[j] = 1
-    for val in st:
-        if is_sosu[val] == 0:
-            answer+=1
+    visit = [0] * len(num_list)
     
-    
-    return answer
+    def dfs(idx):
+        nonlocal num
+        if len(num) == len(numbers):
+            if is_prime[int(num)] == 1: st.add(int(num))
+            return
+        else:
+            if num != "" and is_prime[int(num)] == 1: 
+                st.add(int(num))
+            for i in range(len(num_list)):
+                if visit[i] == 0:
+                    num+=num_list[i]
+                    visit[i] = 1
+                    dfs(i)
+                    visit[i] = 0
+                    num = num[:-1]
+                    
+    for i in range(len(num_list)):
+        visit[i] = 1
+        num = num_list[i]
+        dfs(i)
+        visit[i] = 0
+    return len(st)
