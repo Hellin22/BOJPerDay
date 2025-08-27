141d0f1afd94f8f9b9ec5c26fca995be15642f7d
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-27 14:21:56 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 993.55 ms, Memory: 256 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,3 +1,27 @@
-def solution(sequence, k):
-    answer = []
-    return answer
+def solution(numbers, k):
+    '''
+    1. 오름차순 정렬
+    2. 부분 수열의 원소 모두 포함해야함.
+    3. 합은 k
+    4. 길이 가장 짧아야함.
+    5. 길이 같으면 시작 idx 작은걸로
+    
+    투포인터
+    '''
+    l, r = 0, 0
+    summ = numbers[0]
+    ans = []
+    while l <= r and r < len(numbers):
+        # 1. summ이 k보다 작다. -> r+1
+        if summ <= k:
+            if summ == k:
+                ans.append((l, r))
+            r+=1
+            if r == len(numbers): break
+            summ+=numbers[r]
+        # 2. summ이 k보다 크다 -> l+1
+        if summ > k:
+            summ-=numbers[l]
+            l+=1
+    ans.sort(key = lambda x: (x[1]-x[0], x[0]))
+    return ans[0]
