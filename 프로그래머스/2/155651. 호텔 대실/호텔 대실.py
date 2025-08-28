186b052dd0cddc6f37cf2f7ef3bdb8748bd97fe0
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-28 15:42:43 +0900

    [level 2] Title: 호텔 대실, Time: 40.04 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,39 +1,48 @@
-'''
-1. book_time 분으로 처리
-2. book_time 시작시간 별로 정렬
-3. llist에 추가해야함 (종료시간)
-4. 어떤걸 추가할때에 llist를 돌면서 종료시간+10을 했는데 그게 현재 시작시간보다 작다 == 빼고 넣ㅇ르 수 있음
-1000개니까 1000*1000 -> 가능할거로 예상
-'''
-def solution(book_time):
-    answer = 0
-    # 1. 분으로 처리
-    for i, val in enumerate(book_time):
-        a, b = val[0], val[1]
-        abun = int(a[0]) * 600 + int(a[1]) * 60 + int(a[3]) * 10 + int(a[4])
-        bbun = int(b[0]) * 600 + int(b[1]) * 60 + int(b[3]) * 10 + int(b[4])
-        book_time[i] = [abun, bbun]
-        
-    # 2. 앞에껄로 정렬
-    book_time.sort(key = lambda x: (x[0], x[1]))
+def solution(times):
     
-    # 3. llist에 추가하기
-    llist = [0]
-    for idx, val in enumerate(book_time):
-        flag = True
-        for i in range(len(llist)):
-            if llist[i] + 10 <= val[0]:
-            # 종료시간이 들어있는데 이거 + 10을 했는데 현재 시작시간이 더 크다?
-            # 빼고 현재꺼 넣을 수 있는것
-                llist[i] = val[1]
-                flag = False
-                break
-        if flag: # 새로운 리스트를 만들어서 추가해야함.
-            llist.append(val[1])
+    '''
+    최대 1000개니까 완탐 가능할듯
+    10분간 사용 불가능
+    
+    1. 모두 분으로 바꾸기
+    
+    times를 돌면서 시작하기 전에
+    배열을 돌면서 뺄 수 있는 곳은 빼주기
+    
+    그다음 아무데나 넣기
+    if 아무데나 못넣는다 -> append해서 크기 늘려주기
+    
+    10분간 청소해야하니까 끝 시간에 +10 하기
+    
+    '''
+    times.sort(key = lambda x: x[0])
+    
+    def cht_minute(sttr):
+        return int(sttr[:2]) * 60 + int(sttr[3:])
     
     
+    # 1.분으로 변환
+    for i in range(len(times)):
+        fir, sec = times[i]
+        times[i] = [cht_minute(fir), cht_minute(sec)+10]
     
-    
-    
-    
-    return len(llist)
+    rooms = []
+    # 2. times를 돌면서 진행
+    for fir, sec in times:
+        if rooms:
+            cnt = 0
+            for j in range(len(rooms)):
+                # rooms에서 빠지는게 있으면 빼기
+                if not rooms[j] or rooms[j][1] <= fir:
+                    rooms[j] = []
+                    cnt+=1
+            if cnt == 0:
+                rooms.append([fir, sec])
+            else:
+                for j in range(len(rooms)):
+                    if rooms[j] == []:
+                        rooms[j] = [fir, sec]
+                        break
+        else:
+            rooms.append([fir, sec])
+    return len(rooms)    
