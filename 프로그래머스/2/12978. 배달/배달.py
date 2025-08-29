15a84f0c6d2504abb2652ff668ba703abb11fd14
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-29 18:15:59 +0900

    [level 2] Title: 배달, Time: 5.04 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,35 +1,34 @@
 from heapq import heappush, heappop
-
-def solution(n, road, K):
-    # 마을개수 n, 도로 road, k시간
-    # 1번마을에서 시작
+def solution(n, road, k):
+    '''
+    출발, 도착, 거리
+    음식배달 가능한 시간 = k
     
+    1번 마을에서 배달
+    '''
     graph = [[] for _ in range(n+1)]
-    for a, b, dist in road:
-        graph[a].append((dist, b))
-        graph[b].append((dist, a))
-        
+    
+    for fr, to, dist in road:
+        graph[fr].append((to, dist))
+        graph[to].append((fr, dist))
+    
+    INF = 987654321
+    dists = [INF] * (n+1)
     hq = []
-    dst = [10000000000000] * (n+1)
-    dst[1] = 0 # 1번마을은 dist 0
-    heappush(hq, (0, 1))
+    
+    heappush(hq, (0, 1)) 
+    dists[1] = 0
     
     while hq:
-        dist, node = heappop(hq)    
+        dist, node = heappop(hq)
         
-        if dist > dst[node]: continue
+        if dists[node] < dist: continue
         
-        for d, a in graph[node]:
-            if dist + d > dst[a]: continue
-            else:
-                heappush(hq, (dist+d, a))
-                dst[a] = dist+d
-    
-    print(dst)
-    answer = 0
-    for di in dst:
-        if di <= K:
-            answer+=1
-    return answer
-    
-    
+        for i in range(len(graph[node])):            
+            if graph[node][i][1] + dist > dists[graph[node][i][0]]: continue
+            
+            heappush(hq, (graph[node][i][1]+dist, graph[node][i][0]))
+            dists[graph[node][i][0]] = graph[node][i][1]+dist
+
+    return len([i for i in dists if i <= k])
+        
