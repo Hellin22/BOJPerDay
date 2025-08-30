776effb24eee38871a2b158590ab73af62f7c403
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-30 22:40:48 +0900

    [level 2] Title: 피보나치 수, Time: 19.49 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -5,7 +5,7 @@ def solution(n):
     fibo_list[0] = 0
     fibo_list[1] = 1
     fibo_list[2] = 1
-    fibo_list[3] = 2
+    fibo_list[3] = 2#
     
     for i in range(2, n+1):
         fibo_list[i] = (fibo_list[i-1] + fibo_list[i-2]) % 1234567
