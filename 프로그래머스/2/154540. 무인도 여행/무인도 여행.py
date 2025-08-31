c4381fb9273f5755f1ac3915ecdcc0328fe76916
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-08-31 16:46:41 +0900

    [level 2] Title: 무인도 여행, Time: 10.24 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.py
@@ -1,40 +1,34 @@
-'''
-전형적인 bfs?
-x가 아니라면 bfs를 돌림 -> 식량 저장할 값 필요 -> bfs의 return 값이 sum_food
-이걸 answer.append 해주기
-
-마지막에 sort하는거 잊지말기
-'''
 from collections import deque
-def solution(maps):
-    answer = []
+def solution(arr):
+    '''
+    X 1~9
+    X는 바다
+    숫자는 무인도
     
-    n = len(maps)
-    m = len(maps[0])    
-    visit = [[0] * m for _ in range(n)]
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    def bfs(i, j):
-        nonlocal n, m
-        food_sum = 0
-        dq = deque()
-        dq.append((i, j))
-        visit[i][j] = 1
-        while dq:
-            x, y = dq.popleft()
-            food_sum+=int(maps[x][y])
-            for dx, dy in directions:
-                nx, ny = x+dx, y+dy
-                if 0<=nx<n and 0<=ny<m and visit[nx][ny] == 0 and maps[nx][ny] != "X":
-                    dq.append((nx, ny))
-                    visit[nx][ny] = 1
-        return food_sum
+    무인도 없으면 -1
+    있으면 오름차순
+    
+    그냥 일반 BFS?
+    '''
+    
+    n, m = len(arr), len(arr[0])
+    ans = []
+    for i in range(n):
+        arr[i] = list(arr[i])
     for i in range(n):
         for j in range(m):
-            if visit[i][j] == 0 and maps[i][j] !="X":
-                # int 적용해줘야함.
-                answer.append(bfs(i, j))
-                print(1)
-    if not answer: return [-1]
-    
-    
-    return sorted(answer)
+            if arr[i][j] != "X":
+                dq = deque([(i, j, int(arr[i][j]))])
+                fin_cnt = int(arr[i][j])
+                arr[i][j] = "X"
+                while dq:
+                    x, y, cnt = dq.popleft()
+                    for dx, dy in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                        nx, ny = x+dx, y+dy
+                        if nx < 0 or ny < 0 or nx >= n or ny >= m or arr[nx][ny] == "X": continue
+                        dq.append((nx,ny,cnt+int(arr[nx][ny])))
+                        fin_cnt = fin_cnt + int(arr[nx][ny])                        
+                        arr[nx][ny] = "X"
+                ans.append(fin_cnt)
+                
+    return sorted(ans) if ans else [-1]
