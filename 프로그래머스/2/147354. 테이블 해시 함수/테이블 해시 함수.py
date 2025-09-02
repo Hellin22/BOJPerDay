519d6c446cba806c618c08f1626af369d91538c8
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-09-02 18:55:16 +0900

    [level 2] Title: 테이블 해시 함수, Time: 94.74 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,41 +1,26 @@
-'''
-data	                            col  row_begin	row_end	 result
-[[2,2,6],[1,5,10],[4,2,9],[3,8,3]]	 2	     2	       3	   4
-
-[[2,2,6],
-[1,5,10],
-[4,2,9],
-[3,8,3]]
-
-즉, row들에 대해서 1. row[col] 오름차순 정렬, 2. row[0] 내림차순 정렬
-그러면 정렬된 튜플이 나올것.
-S_i를 i번째 튜플에 대해 각 컬럼의 값을 i로 나눈 나머지들의 합.
-즉, S_1은 첫번째 튜플에 대햇 각 컬럼을 i로 나눔.
-
-row_begin <= i <= row_end인 모든 S_i를 누적해서 xor 한 값을 가져와라.
-
-'''
-
 def solution(data, col, row_begin, row_end):
-    answer = 0
+    '''
+    행은 튜플
+    첫번째 컬럼은 기본키
     
-    # data를 sort 해야함.
-    data.sort(key = lambda x: (x[col-1], -x[0])) # row[col] 오름, row[0] 내림(-연산)
+    해시 함수는 col, row_begin, row_end 입력받음
     
-    llist = []
-    answer = 0
-    for i in range(row_begin-1, row_end+1-1):
-        
-        namerji_sum = 0
-        for j in range(len(data[i])):  
-            # data[i] # row가 나옴.
-            namerji_sum += data[i][j] % (i+1)
-            # data[i][j]를 각각 i로 나눈 나머지를 더함.
-            # i+1로 나눠야한다.
-        # 이거랑 이전꺼를 xor 해야함.
-        answer = answer^namerji_sum        
+    1. col 번째 컬럼 값 기준 오름차순. 동일? -> 첫번째 컬럼 값으로 내림차순
+    2. 정렬한 후에 S_i는 i번째 튜플에 대해 각 컬럼을 i로 나눈 나머지들의 합
+    3. row_begin <= i <= row_end인 모든 S_i를 누적해서 xor 연산(^)
     
-            
-        
+    (row+1)로 %한 값의 합
+    이때 row는 row_begin ~ row_end
     
-    return answer
+    '''
+    data.sort(key = lambda x: (x[col-1], -x[0]))
+    s_i = 0
+    for i in range(row_begin-1, row_end):
+        summ = 0
+        for j in range(len(data[i])):
+            summ += data[i][j]%(i+1)
+
+        s_i = s_i^summ
+    return s_i
+
+    
