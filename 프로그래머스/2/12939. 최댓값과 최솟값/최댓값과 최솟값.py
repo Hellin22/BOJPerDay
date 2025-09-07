0027ecdcda0299fffa4406b573184bd57cf50003
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-09-07 22:42:17 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,5 +1,5 @@
 '''
-문자열 s -> 구분된 숫자들
+문자열 s -> 구분된 숫자
 
 최소 최대로 구분 (최소) (최대)
 split -> 0,
@@ -7,4 +7,4 @@ split -> 0,
 
 def solution(k):
     splited = sorted(list(map(int, k.split(' '))))
-    return f"{splited[0]} {splited[-1]}"
+    return f"{splited[0]} {splited[-1]}" # f로 하는거 말고 다른 방법도 있는거 추가
