d9aa10fe60a84fc6bbb37e68c33bff4e10704866
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-10-17 14:48:54 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 354.56 ms, Memory: 42.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,26 +1,12 @@
 def solution(elements):
-
+    # 누적합으로 처리
+    # 1개부터 ~ 모든 ele 합 
     st = set()
-    n = len(elements)
-    for i in range(0, n): # 몇개의 원소를 가진 부분수열인지
-        l, r = 0, i
-        wd = sum(elements[l:r+1])
-        st.add(wd)
-        for j in range(1, n):
-            wd-=elements[l]
-            l, r = (l+1) % n, (r+1) % n
-            wd+=elements[r]
-            st.add(wd)
     
-    return len(st)
-
-    '''
-    연속하는 부분수열의 합으로 만들 수 있는 수
-    저장은 set
-    len(elements)까지 반복
-    1개 ~ n-1개 까지 -> return ans + 1
-    윈도우 슬라이싱으로 진행 -> left, right -> 둘다 +1 % n
-    초기값(몇개를 연속으로 더하느냐)이 중요한듯? left가 다시 0이 되면 종료
-    '''
-    
-    
+    for i in range(1, len(elements)): # 개수
+        summ = sum(elements[:i]) # 최초 합 = 개수만큼 # 0 / 0 1 / 0 1 2
+        st.add(summ)
+        for j in range(1, len(elements)): # 시작지점 -> 0번째 제외
+            summ+= elements[(j+i-1) % len(elements)] - elements[j-1]
+            st.add(summ)
+    return len(st)+1
