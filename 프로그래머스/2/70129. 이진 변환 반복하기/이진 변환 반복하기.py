e51f8d39c369a78c4daed562a3bfa90a828d0ad4
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-10-17 14:05:25 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.27 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,19 +1,14 @@
-'''
-x의 모든 0 제거
-x 길이가 c이면 x는 c가 되고 c를 2진법으로 바꾼걸로 바뀜.
-이거 bin?
-1이 될때까지 진행
-replace 0 ''
-'''
 def solution(s):
-    ans, cnt = 0, 0
-    while s != '1':
-        cnt+=1
+    # 이진 변환 횟수 + 제거된 0의 개수
+    # s가 1이 될때까지
+    ans = [0, 0]
+    while s != "1":
+        ori_len = len(s)
+        s = s.replace("0", "")
+        change_len = len(s)
+        ans[0] += 1
+        ans[1] += ori_len - change_len
+        s = bin(len(s))[2:]        
         
-        # 1. s의 모든 0 제거
-        ans += len(s) - len(s.replace('0', ''))
-        s = s.replace('0', '')
         
-        # 2. s의 길이를 다시 s에 대입
-        s = bin(len(s))[2:]
-    return [cnt, ans]
+    return ans
