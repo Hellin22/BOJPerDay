dfd36497bda5902eb1516f8c444f4e171e43b65b
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-10-17 15:44:24 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.36 MB, 시간: 0.48 ms
+메모리: 9.32 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 17:24:29
+2025년 10월 17일 15:44:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,22 @@
+from collections import deque
 def solution(progresses, speeds):
-    answer = []
     
-    # 며칠 걸리는지는 상관없음
-    n = len(speeds)
-    cur = 0 # 0번째 부터 speeds만큼 progresses 증가시키기
-    while cur < n:
-        for i in range(cur, n):
-            # cur ~ n-1까지 모두 speeds만큼 progresses 증가시키기
-            if progresses[i] < 100: # 100보다 작다면
-                progresses[i]+=speeds[i] # speeds만큼 증가
-        # 만약 progresses[cur]이 100보다 커진다면? answer에 추가할 준비
+    ans = []
+    dq = deque()
+    # 현재 날짜 -> 1일 2일 3일 ...
+    cur = 1
+    
+    for a, b in zip(progresses, speeds):
+        dq.append((a, b))
         
-        if progresses[cur] >= 100:
+    while dq: 
+        if dq[0][0] + dq[0][1] * cur < 100:
+            cur+=1
+        else:
             cnt = 0
-            while cur < n:
-                if progresses[cur] < 100:
-                    break
+            while dq and dq[0][0]+dq[0][1]*cur >= 100:
+                dq.popleft()
                 cnt+=1
-                cur+=1
-                    
-            answer.append(cnt)
-    return answer
+            ans.append(cnt)
+    
+    return ans
