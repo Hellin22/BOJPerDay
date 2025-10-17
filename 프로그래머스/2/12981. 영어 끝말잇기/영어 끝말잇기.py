4187da3af3736c3323986264be46baa13b648dcb
KyeongSeok Baek <95176614+Hellin22@users.noreply.github.com>
2025-10-17 14:35:56 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,15 +1,12 @@
 def solution(n, words):
+    # 번호, 차례 (1번사람, 3번째)
     st = set()
-    for i, word in enumerate(words):
-        if (i != 0 and words[i-1][-1] != words[i][0]) or word in st or len(word) == 1: # 실패
-            return [i%n+1, i//n+1]
-        st.add(word)
+    for i, word in enumerate(words): # n이 3 -> 0 1 2 / 3 4 5 / ...
+        if i == 0:
+            st.add(word)
+        else:
+            if words[i-1][-1] != word[0] or word in st: # 틀리거나 중복
+                return [i % n +1, i//n + 1]
+            else:
+                st.add(word)
     return [0, 0]
-    
-    '''
-    n명 끝말잇기 -> 1 ~ n번
-    
-    순환 구조 + 앞선사람의 마지막 문자로 시작 + 이전 단어 사용 안됨 + 한글자 안됨
-    
-    가장 먼저 탈락하는 사람 번호 + 몇번째 차례에 탈락?
-    '''
